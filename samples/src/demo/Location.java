a8b2ee960455be23b64d3ed04290fb8ac66578b9
Nurin Syafiqah Binti Mohd Saofi <nmuslimah84@gmail.com>
2021-03-18 15:42:43 +0800

    modify location

--- samples/src/demo/Location.java
+++ samples/src/demo/Location.java
@@ -3,12 +3,26 @@ package demo;
 public class Location {
 	
 	private String name;
+	private int locationId;
+	private int description;
 	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public int getLocationId() {
+		return locationId;
+	}
+	public void setLocationId(int locationId) {
+		this.locationId = locationId;
+	}
+	public int getDescription() {
+		return description;
+	}
+	public void setDescription(int description) {
+		this.description = description;
 	}
 	
 }
